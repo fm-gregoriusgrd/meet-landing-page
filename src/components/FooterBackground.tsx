bac50472f8c066d0db60a3a996de07d345d74e33
Gregorius G <gregorius.gerald21@gmail.com>
2025-07-25 23:13:52 +0700

    add hover state

--- src/components/FooterBackground.tsx
+++ src/components/FooterBackground.tsx
@@ -6,7 +6,6 @@ export default function FooterBackground({ className }: ImageProps) {
   return (
     <div className={`${className} bg-cover bg-center px-8 py-16`}>
       <div className="mx-auto flex max-w-[500px] flex-col items-center justify-center gap-6 text-center text-white lg:max-w-[1120px] lg:flex-row lg:text-start">
-        
         <h2 className="text-3xl font-black md:text-4xl lg:max-w-[400px]">
           Experience more together
         </h2>
@@ -16,7 +15,7 @@ export default function FooterBackground({ className }: ImageProps) {
           group video sessions.
         </p>
 
-        <button className="h-14 w-48 cursor-pointer rounded-full bg-[var(--custom-purple-600)] leading-6 font-black text-white">
+        <button className="h-14 w-48 cursor-pointer rounded-full bg-[var(--custom-purple-600)] leading-6 font-black text-white hover:bg-[var(--custom-purple-500)]">
           Download{" "}
           <span className="pl-1 text-[var(--custom-purple-300)]">v1.3</span>
         </button>
--- src/components/HeroContent.tsx
+++ src/components/HeroContent.tsx
@@ -15,11 +15,11 @@ export default function HeroContent({ className = "" }: HeroContentProps) {
         collaborate across any device.
       </p>
       <div className="flex flex-col items-center gap-4 md:flex-row">
-        <button className="h-14 w-48 rounded-4xl bg-[var(--custom-cyan-600)] leading-6 font-black text-white">
+        <button className="h-14 w-48 cursor-pointer rounded-4xl bg-[var(--custom-cyan-600)] leading-6 font-black text-white hover:bg-[var(--custom-cyan-500)]">
           Download
           <span className="pl-1 text-[var(--custom-cyan-300)]">v1.3</span>
         </button>
-        <button className="h-14 w-40 rounded-4xl bg-[var(--custom-purple-600)] font-black text-white">
+        <button className="h-14 w-40 cursor-pointer rounded-4xl bg-[var(--custom-purple-600)] font-black text-white hover:bg-[var(--custom-purple-500)]">
           What is it?
         </button>
       </div>
